005c350606e8c77cdfdabf1689992cfddf46a773
poojapauskar <git.poojapauskar@gmail.com>
2016-02-15 15:48:47 +0530

    added payment gateway and database updating

--- order/models.py
+++ order/models.py
@@ -5,6 +5,7 @@ from django.core.validators import RegexValidator
 
 class Order(models.Model):
     created = models.DateTimeField(auto_now_add=True)
+    transition_id = models.CharField(max_length=100, blank=False, default='')
     order_id = models.CharField(max_length=100, blank=False, default='')
     user_id = models.CharField(max_length=100, blank=False, default='')
     status = models.CharField(max_length=100, blank=False, default='')
--- user_data/models.py
+++ user_data/models.py
@@ -3,6 +3,10 @@ from django.db import models
 
 from django.core.validators import RegexValidator
 
+
+
+
+
 class User_data(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=100, blank=False, default='')
